5b9c1fbb213c93ca5a8ea456c9b94c849ef5c330
Emil B <44379110+Ixaruz@users.noreply.github.com>
2023-07-12 21:51:27 +0200

    some updates..
    ...to mimic animal crossing's behavior

--- nintendo/acbaa.py
+++ nintendo/acbaa.py
@@ -2,6 +2,9 @@ from anynet import tls, http
 import pkg_resources
 import ormsgpack
 import urllib.parse
+from time import sleep
+from random import uniform
+import json
 
 import logging
 logger = logging.getLogger(__name__)
@@ -58,10 +61,10 @@ class ACBAAClient:
 		if token:
 			req.headers["Authorization"] = "Bearer " + token
 			req.headers["Content-Type"] = "application/x-msgpack"
-		if req.method == "POST":
-			if len(req.body) != 88:
-				raise ValueError("ID and or Password doesn't have the right size")
-			req.headers["Content-Length"] = 88
+		if req.method == "POST" or req.method == "PUT":
+			if req.body is None:
+				raise ValueError("req.body can't be empty when using POST")
+			req.headers["Content-Length"] = len(req.body)
 		if module != MODULE_ENS:
 			raise NotImplementedError("Unknown Module")
 		
@@ -78,12 +81,12 @@ class ACBAAClient:
 			"id": id,
 			"password": password
 		}
-		#print(data)
 		#this has to be hacky, because I couldn't get proper formatting of the id in the msgpack (uint64 <-> int64; check https://github.com/msgpack/msgpack/blob/master/spec.md)
 		req.body = ormsgpack.packb(data).replace(b'\xcf',b'\xd3')
+		if len(req.body) != 88:
+			raise ValueError("ID and or Password doesn't have the right size")
 		response = await self.request(req, access_token, MODULE_ENS)
 		self.auth_token = ormsgpack.unpackb(response.body)["token"]
-		#print(self.auth_token)
 		return response.body
 	
 	async def search_dream_by_id(self, dream_id):
@@ -101,46 +104,59 @@ class ACBAAClient:
 		url_parts = urllib.parse.urlparse(url)
 		query = dict(urllib.parse.parse_qsl(url_parts.query))
 		query.update(string_query)
-
-		new_url = url_parts._replace(query=urllib.parse.urlencode(query)).geturl()
-
-		req = http.HTTPRequest.get(new_url)
-
-		response = await self.request(req, self.auth_token, MODULE_ENS)
-		print(ormsgpack.unpackb(response.body))
-		return response.body
-	
-	async def download_dream(self, dream_id):
-		if self.auth_token == "":
-			return
-		
-		url = "/api/v1/dream_lands"
-		
-		string_query = {
-			"offset" : 0,
-			"limit" : 150,
-			"q[id]" : dream_id
-		}
-
-		url_parts = urllib.parse.urlparse(url)
-		query = dict(urllib.parse.parse_qsl(url_parts.query))
-		query.update(string_query)
-
 		new_url = url_parts._replace(query=urllib.parse.urlencode(query)).geturl()
 
 		req = http.HTTPRequest.get(new_url)
 
 		response = await self.request(req, self.auth_token, MODULE_ENS)
 		searchresult = ormsgpack.unpackb(response.body)
-
-		if(searchresult["count"] != 1):
-			return 0
+		return searchresult
+	
+	async def download_dream_by_id(self, dream_id):
+		# mimic the game's behavior; it searches and gets metadata, 
+		# then sends feedback and waits for user input
+		# to confirm and search again to download the metadata and dream properly
+		searchresult = await self.search_dream_by_id(dream_id)
+		http.HTTPRequest.get(searchresult["dreams"][0]["meta"])
+		req = await self.send_feedback_id_search(dream_id)
+		sleep(uniform(5.0, 12.0))
+		searchresult = await self.search_dream_by_id(dream_id)
 		
-		req1 = http.HTTPRequest.get(searchresult["dreams"][0]["contents"][0]["url"])
-		BodyData = await self.request(req1, None, MODULE_ENS)
-		#print(searchresult["dreams"][0]["contents"][0]["url"])
-		req2 = http.HTTPRequest.get(searchresult["dreams"][0]["meta"])
-		meta = await self.request(req2, None, MODULE_ENS)
+		#if not one dream is found
+		if(searchresult["count"] != 1):
+			logger.warning("Search for dream address ended with an unexpected response")
+			return 0, 0
+		
+		logger.info(json.dumps(searchresult, indent=2))
+
+		req1 = http.HTTPRequest.get(searchresult["dreams"][0]["meta"])
+		meta = await self.request(req1, None, MODULE_ENS)
+		req2 = http.HTTPRequest.get(searchresult["dreams"][0]["contents"][0]["url"])
+		BodyData = await self.request(req2, None, MODULE_ENS)
 
 		return BodyData.body, ormsgpack.unpackb(meta.body)
+
+	# idk how necessary this function is, though it's best to send feedback after searching for a dream
+	async def send_feedback_id_search(self, dream_id):
+		if self.auth_token == "":
+			return
+		
+		req = http.HTTPRequest.post("/api/v1/dream_lands/%s/feedback" %dream_id)
+		
+		data = {
+			"kind" : "id_search"
+		}
+
+		req.body = ormsgpack.packb(data)
+		response = await self.request(req, self.auth_token, MODULE_ENS)
+		return response
+	
+	async def get_friends(self):
+		if self.auth_token == "":
+			return
+		
+		req = http.HTTPRequest.get("/api/v1/friends")
+		response = await self.request(req, self.auth_token, MODULE_ENS)
+		friends = ormsgpack.unpackb(response.body)
+		return friends
         

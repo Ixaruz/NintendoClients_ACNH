5d620ed325fab9c4d4cedb0ee488631f9d4317ad
Emil B <44379110+Ixaruz@users.noreply.github.com>
2023-11-12 11:59:11 +0100

    Update acbaa example

--- examples/switch/animalcrossing_dream.py
+++ examples/switch/animalcrossing_dream.py
@@ -1,10 +1,4 @@
-
-from nintendo.baas import BAASClient
-from nintendo.dauth import DAuthClient
-from nintendo.aauth import AAuthClient
-from nintendo.dragons import DragonsClient
-from nintendo.acbaa import ACBAAClient
-from nintendo.nex import backend, authentication, matchmaking, settings
+from nintendo.switch import dauth, aauth, baas, dragons, acbaa
 from nintendo import switch
 import anyio
 import os
@@ -13,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-SYSTEM_VERSION = 1603 #16.0.3
+SYSTEM_VERSION = 1700 #17.0.0
 
 # You can get your user id and password from
 # su/baas/<guid>.dat in save folder 8000000000000010.
@@ -38,12 +32,8 @@ PATH_PRODINFO = "/path/to/PRODINFO"
 ELICENSE_ID = "..." # 32 hex digits
 NA_ID = 0x0123456789abcdef # 16 hex digits
 
-DREAM_ID = 0
-DREAM_ID_TEXT = 'DA-' + '-'.join([f'{DREAM_ID:012d}'[index:index+4]for index in range(0, 12, 4)])
-
 TITLE_ID = 0x01006F8002326000
 TITLE_VERSION = 0x001C0000
-
 
 # can be found in the decrypted main.dat or personal.dat
 # of your animal crossing save file.
@@ -63,6 +53,8 @@ TITLE_VERSION = 0x001C0000
 ACBAA_ID = 0x0123456789abcdef # 16 hex digits also known as mMtNsaId
 ACBAA_PASSWORD = "..." # 64 hex digits
 
+DREAM_ADDRESS = "DA-XXXX-XXXX-XXXX"
+
 async def main():
 	keys = switch.load_keys(PATH_KEYS)
 	
@@ -70,63 +62,69 @@ async def main():
 	cert = info.get_tls_cert()
 	pkey = info.get_tls_key()
 	
-	dauth = DAuthClient(keys)
-	dauth.set_certificate(cert, pkey)
-	dauth.set_system_version(SYSTEM_VERSION)
+	dauth_client = dauth.DAuthClient(keys)
+	dauth_client.set_certificate(cert, pkey)
+	dauth_client.set_system_version(SYSTEM_VERSION)
 	
-	dragons = DragonsClient()
-	dragons.set_certificate(cert, pkey)
-	dragons.set_system_version(SYSTEM_VERSION)
+	dragons_client = dragons.DragonsClient()
+	dragons_client.set_certificate(cert, pkey)
+	dragons_client.set_system_version(SYSTEM_VERSION)
 	
-	aauth = AAuthClient()
-	aauth.set_system_version(SYSTEM_VERSION)
+	aauth_client = aauth.AAuthClient()
+	aauth_client.set_system_version(SYSTEM_VERSION)
 	
-	baas = BAASClient()
-	baas.set_system_version(SYSTEM_VERSION)
+	baas_client = baas.BAASClient()
+	baas_client.set_system_version(SYSTEM_VERSION)
 	
-	acbaa = ACBAAClient()
-	acbaa.set_title_version(TITLE_VERSION)
+	acbaa_client = acbaa.ACBAAClient()
+	acbaa_client.set_title_version(TITLE_VERSION)
 
 	# Request a device authentication token for dragons
-	response = await dauth.device_token(dauth.DRAGONS)
+	response = await dauth_client.device_token(dauth.CLIENT_ID_DRAGONS)
 	device_token_dragons = response["device_auth_token"]
 	
 	# Request a device authentication token for aauth and bass
-	response = await dauth.device_token(dauth.BAAS)
+	response = await dauth_client.device_token(dauth.CLIENT_ID_BAAS)
 	device_token_baas = response["device_auth_token"]
 	
 	# Request a contents authorization token from dragons
-	response = await dragons.contents_authorization_token_for_aauth(device_token_dragons, ELICENSE_ID, NA_ID, TITLE_ID)
+	response = await dragons_client.contents_authorization_token_for_aauth(device_token_dragons, ELICENSE_ID, NA_ID, TITLE_ID)
 	contents_token = response["contents_authorization_token"]
 	
 	# Request an application authentication token
-	response = await aauth.auth_digital(TITLE_ID, TITLE_VERSION, device_token_baas, contents_token)
+	response = await aauth_client.auth_digital(TITLE_ID, TITLE_VERSION, device_token_baas, contents_token)
 	app_token = response["application_auth_token"]
 	
 	# Request an anonymous access token for baas
-	response = await baas.authenticate(device_token_baas)
+	response = await baas_client.authenticate(device_token_baas)
 	access_token = response["accessToken"]
 	
 	# Log in on the baas server
-	response = await baas.login(
+	response = await baas_client.login(
 		BAAS_USER_ID, BAAS_PASSWORD, access_token, app_token
 	)
-
 	user_id = int(response["user"]["id"], 16)
 	id_token = response["idToken"]
 
-	response = await acbaa.authenticate(ACBAA_ID, ACBAA_PASSWORD, id_token)
+	response = await acbaa_client.authenticate(ACBAA_ID, ACBAA_PASSWORD, id_token)
 
-	Body, meta = await acbaa.download_dream_by_id(DREAM_ID)
+	DREAM_ID_TEXT = ""
+	for c in DREAM_ADDRESS:
+		if c.isdigit():
+			DREAM_ID_TEXT = DREAM_ID_TEXT + c
+		
+	DREAM_ID = int(DREAM_ID_TEXT)
+	
+	Body, meta = await acbaa_client.download_dream_by_id(DREAM_ID)
 
 
-	if not (os.path.isdir(DREAM_ID_TEXT)): os.mkdir(DREAM_ID_TEXT)
+	if not (os.path.isdir(DREAM_ADDRESS)): os.mkdir(DREAM_ADDRESS)
 
-	f = open(DREAM_ID_TEXT + "/dreamdownload.dat", "wb")
+	f = open(DREAM_ADDRESS + "/dreamdownload.dat", "wb")
 	f.write(Body)
 	f.close()
 
-	f = open(DREAM_ID_TEXT + "/dreammeta.txt", "w")
+	f = open(DREAM_ADDRESS + "/dreammeta.txt", "w")
 	f.write(str(meta))
 	f.close()
 	
